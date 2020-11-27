2f32d4f4b30338f64b7b019348abe56dbd426b51
Alberto Trevisan <contato@albertotrevisan.com.br>
2020-11-27 17:09:03 -0300

    show history

--- customers/view.php
+++ customers/view.php
@@ -2,6 +2,20 @@
 ini_set('display_errors',1);
 	require_once('functions.php'); 
 	view($_GET['id']);
+	$db = open_database();
+	$id = filter_input(INPUT_GET,'id',FILTER_SANITIZE_NUMBER_INT);
+	$sql = "SELECT * FROM veiculos WHERE id_cliente = '$id'";
+	$query = $db->query($sql);
+	$veiculos = [];
+	while($row = $query->fetch_assoc()) {
+		$veiculos[]=$row;
+	}
+	$sql2 = "SELECT * FROM os LEFT JOIN veiculos ON os.id_veiculo=veiculos.id WHERE veiculos.id_cliente = '$id'";
+	$query = $db->query($sql2);
+	$oss = [];
+	while($row = $query->fetch_assoc()) {
+		$oss[]=$row;
+	}
 ?>
 
 <?php include(HEADER_TEMPLATE); ?>
@@ -64,5 +78,55 @@ ini_set('display_errors',1);
 	  <a href="index.php" class="btn btn-default">Voltar</a>
 	</div>
 </div>
+
+<br>
+<?php if(count($veiculos) > 0) { ?>
+	<p style="padding-bottom: 1rem;border-bottom: 1px solid grey;" class="display h4"><a href="#" onclick="$('#table1').toggle();$(this).toggleClass('text-success');" class="btn btn-primary">&#128065;</a>&nbsp;Veículos</p>
+	<table class="table table-hover" id="table1" style="display:none">
+		<thead class="thead-light">
+			<tr>
+				<th scope="col">Marca</th>
+				<th scope="col">Modelo</th>
+				<th scope="col">Placa</th>
+				<th scope="col" class="text-right">Ações</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($veiculos as $veiculo) { ?>
+				<tr>
+					<td><?=$veiculo['marca']?></td>
+					<td><?=$veiculo['modelo']?></td>
+					<td><?=$veiculo['placa']?></td>
+					<td align="right"><a href="<?=BASEURL?>veiculos/view.php?id=<?=$veiculo['id']?>" class="btn btn-success">Visualizar</a></td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+<?php } ?>
+
+<br>
+<?php if(count($oss) > 0) { ?>
+	<p style="padding-bottom: 1rem;border-bottom: 1px solid grey;" class="display h4"><a href="#" onclick="$('#table2').toggle();$(this).toggleClass('text-success');" class="btn btn-primary">&#128065;</a>&nbsp;Ordens de Serviços</p>
+	<table class="table table-hover" id="table2" style="display:none">
+		<thead class="thead-light">
+			<tr>
+				<th scope="col">Placa</th>
+				<th scope="col">Dinheiro</th>
+				<th scope="col">Data de Registro</th>
+				<th scope="col" class="text-right">Ações</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($oss as $veiculo) { ?>
+				<tr>
+					<td><?=$veiculo['placa']?></td>
+					<td>R$ <?=number_format($veiculo['dinheiro'],2,',','.')?></td>
+					<td><?php $today = new DateTime($veiculo['dt_registro']); echo $today->format("d/m/Y"); ?></td>
+					<td align="right"><a href="<?=BASEURL?>os/view.php?id=<?=$veiculo['id']?>" class="btn btn-success">Visualizar</a></td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+<?php } ?>
 
 <?php include(FOOTER_TEMPLATE); ?>
--- veiculos/view.php
+++ veiculos/view.php
@@ -2,6 +2,14 @@
 ini_set('display_errors',1);
 	require_once('functions.php'); 
 	view($_GET['id']);
+	$id = filter_input(INPUT_GET,'id',FILTER_SANITIZE_NUMBER_INT);
+	$db = open_database();
+	$sql2 = "SELECT * FROM os WHERE id_veiculo = '$id'";
+	$query = $db->query($sql2);
+	$oss = [];
+	while($row = $query->fetch_assoc()) {
+		$oss[]=$row;
+	}
 ?>
 
 <?php include(HEADER_TEMPLATE); ?>
@@ -51,5 +59,35 @@ ini_set('display_errors',1);
 	  <a href="index.php" class="btn btn-default">Voltar</a>
 	</div>
 </div>
+
+<?php if(count($oss) > 0) { ?>
+	<p style="padding-bottom: 1rem;border-bottom: 1px solid grey;" class="display h4"><a href="#" onclick="$('#table2').toggle();$(this).toggleClass('text-success');" class="btn btn-primary">&#128065;</a>&nbsp;Veículos</p>
+	<table class="table table-hover" id="table2" style="display:none">
+		<thead class="thead-light">
+			<tr>
+				<th scope="col">Serviços</th>
+				<th scope="col">Valor Cobrado</th>
+				<th scope="col">Data de Registro</th>
+				<th scope="col" class="text-right">Ações</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($oss as $veiculo) { ?>
+				<tr>
+					<td><?php 
+						$services_list = [];
+						foreach(json_decode($veiculo['services'],true) as $servico) {
+							$services_list[]=$servico['servico'];
+						}
+						echo join(", ",$services_list);
+					?></td>
+					<td>R$ <?=number_format($veiculo['dinheiro'],2,',','.')?></td>
+					<td><?php $today = new DateTime($veiculo['dt_registro']); echo $today->format("d/m/Y"); ?></td>
+					<td align="right"><a href="<?=BASEURL?>os/view.php?id=<?=$veiculo['id']?>" class="btn btn-success">Visualizar</a></td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+<?php } ?>
 
 <?php include(FOOTER_TEMPLATE); ?>
